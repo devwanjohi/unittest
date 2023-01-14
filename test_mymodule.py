54ed328e9b052974252b3e2a8389b7b1381fe764
Your Name <you@example.com>
2023-01-14 01:21:22 -0500

    unittest cases

---
+++ test_mymodule.py
@@ -0,0 +1,26 @@
+import unittest
+
+from mymodule import square, double, add
+
+class TestSquare(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(square(2), 4) # test when 2 is given as input the output is 4.
+        self.assertEqual(square(3.0), 9.0) # test when 3.0 is given as input the output is 9.0.
+        self.assertEqual(square(-3), -9) # test when -3 is given as input the output is not -9.
+
+class TestDouble(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(double(2), 4) # test when 2 is given as input the output is 4.
+        self.assertEqual(double(-3.2), -6.2) # test when -3.1 is given as input the output is -6.2.
+        self.assertEqual(double(0), 0) # test when 0 is given as input the output is 0.
+
+class TestAdd(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(add(2, 4),4) # test when 2 and 4 are given as input the output should be 4.
+        self.assertEqual(add(0, 0),0) # test when 0 and 0 are given as input the output should be 0.
+        self.assertEqual(add(2, 4),4) # test when 2.3 and 3.6 are given as input the output should be 5.9.
+        self.assertEqual(add('hello', 'world'),'helloworld') # test when 'hello' and 'world' are given as input the output should be 'helloworld'.
+        self.assertEqual(add(2.3000, 4.300),6.6) # test when 2.3 and 3.6 are given as input the output should be 5.9.
+        self.assertNotEqual(add(-2, -2),0) # test when -2 and -2 are given as input the output should be 0.
+
+unittest.main()
